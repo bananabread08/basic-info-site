0216776ad7a7a4e5216594fa3d4e2e5e16ba19cf
bananabread08 <rmkfermin@gmail.com>
2022-04-18 15:57:58 +0800

    create pages and index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('https');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(() => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello, World</h1>');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
