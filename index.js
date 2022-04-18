b15e8fa6f16162169eea57921ef2abdd1f1fbcba
bananabread08 <rmkfermin@gmail.com>
2022-04-18 16:57:10 +0800

    move paths to pages, add redirect links

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const http = require('https');
+const http = require('http');
 const fs = require('fs');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-const server = http.createServer(() => {
-  res.statusCode = 200;
+const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello, World</h1>');
+  let path = './pages/';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/contact':
+      path += 'contact.html';
+      res.statusCode = 200;
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+
+  //send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`);
-});
+server.listen(port, () => console.log(`Server running at port ${port}`));
